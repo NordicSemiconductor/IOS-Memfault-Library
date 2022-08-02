cb8472ccb71bc168d04591b89c4f36ebaf163584
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2022-08-02 12:57:34 +0100

    Git Init

---
+++ Shared/nRF_MemfaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  nRF_MemfaultApp.swift
+//  Shared
+//
+//  Created by Dinesh Harjani on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct nRF_MemfaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
