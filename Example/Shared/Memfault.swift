cd5d8f31f68a414907323aa3ea787d9b12ad0ea9
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2022-08-26 09:47:54 +0100

    Trying to prepare for Memfault Library split
    Don't necessarily know exactly what I'm doing.

---
+++ Example/Shared/Memfault.swift
@@ -0,0 +1,21 @@
+//
+//  Memfault.swift
+//  nRF Memfault
+//
+//  Created by Dinesh Harjani on 25/8/22.
+//
+
+import Foundation
+import iOS_BLE_Library
+
+final class Memfault {
+    
+    func connect<T: BluetoothDevice>(to device: T) {
+        // To-Do
+    }
+    
+    func disconnect<T: BluetoothDevice>(from device: T) {
+        // To-Do
+    }
+}
+
--- Example/Shared/State/AppData.swift
+++ Example/Shared/State/AppData.swift
@@ -299,14 +299,3 @@ private extension AppData {
         }
     }
 }
-
-// MARK: - Private
-
-private extension CBUUID {
-    
-    static let MDS = CBUUID(string: "54220000-F6A5-4007-A371-722F4EBD8436")
-    static let MDSDeviceIdentifierCharacteristic = CBUUID(string: "54220002-f6a5-4007-a371-722f4ebd8436")
-    static let MDSDataURICharacteristic = CBUUID(string: "54220003-f6a5-4007-a371-722f4ebd8436")
-    static let MDSAuthCharacteristic = CBUUID(string: "54220004-f6a5-4007-a371-722f4ebd8436")
-    static let MDSDataExportCharacteristic = CBUUID(string: "54220005-f6a5-4007-a371-722f4ebd8436")
-}
