d7236355a136630ed31f3ad2f606cf3238838540
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2022-08-31 12:46:07 +0100

    macOS Build now Builds & Runs, though it doesn't do much
    We shifted development towards iOS, since that was the primary target, and now we can't 'connect' from the Mac.

--- Example/Shared/Views/Device/ChunkView.swift
+++ Example/Shared/Views/Device/ChunkView.swift
@@ -104,7 +104,11 @@ struct ChunkView: View {
         }
         .contextMenu {
             Button(action: {
+                #if os(macOS)
+                NSPasteboard.general.setString(chunk.data.hexEncodedString(), forType: .string)
+                #else
                 UIPasteboard.general.string = chunk.data.hexEncodedString()
+                #endif
             }) {
                 Text("Copy to clipboard")
                 Image(systemName: "doc.on.doc")
